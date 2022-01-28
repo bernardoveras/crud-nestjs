0939cf8a6e72ee0468110a8b3a0935ec2ba539cd
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-01-28 01:02:58 -0300

    Add MONGO_URL config on .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL), UsersModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.env` }),
+    MongooseModule.forRoot(process.env.MONGO_URL), 
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
